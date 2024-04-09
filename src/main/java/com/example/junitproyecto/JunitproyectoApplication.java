62f515f9e956f35fc0d5c710f613da51237e27eb
larinpaul <www.larinpavel@mail.ru>
2024-04-09 08:44:49 +0500

    First commit

---
+++ src/main/java/com/example/junitproyecto/JunitproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.junitproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitproyectoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitproyectoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junitproyecto/JunitproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitproyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
