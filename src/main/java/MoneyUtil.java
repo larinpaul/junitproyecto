1b5f2dc50cc6d0e3c6733dfda5cecbb41e8329cd
larinpaul <www.larinpavel@mail.ru>
2024-04-12 12:03:28 +0500

    Trying to fix MoneyUtil.format(double money) {}

--- src/main/java/MoneyUtil.java
+++ src/main/java/MoneyUtil.java
@@ -3,8 +3,15 @@ import java.math.BigDecimal;
 public class MoneyUtil {
 
     public static String format(double money) {
+        String symbol = "$";
+        if (money < 0) {
+            symbol = "-$";
+        }
+        else {
+            symbol = "$";
+        }
         BigDecimal rounded = BigDecimal.valueOf(money).setScale(2, BigDecimal.ROUND_HALF_EVEN); // Decprecated, better use RoundingMode.HALF_EVEN
-        return "$" + rounded;
+        return symbol + rounded; // -$-1000
     }
 
 }
