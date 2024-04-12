dc2f94be2c830db372242737c1b1aa2ff73e639b
larinpaul <www.larinpavel@mail.ru>
2024-04-12 12:04:19 +0500

    Fixed MoneyUtil.format(double money) {}

--- src/main/java/MoneyUtil.java
+++ src/main/java/MoneyUtil.java
@@ -6,12 +6,13 @@ public class MoneyUtil {
         String symbol = "$";
         if (money < 0) {
             symbol = "-$";
+            money = money * (-1);
         }
         else {
             symbol = "$";
         }
         BigDecimal rounded = BigDecimal.valueOf(money).setScale(2, BigDecimal.ROUND_HALF_EVEN); // Decprecated, better use RoundingMode.HALF_EVEN
-        return symbol + rounded; // -$-1000
+        return symbol + rounded;
     }
 
 }
